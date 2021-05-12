963f1bb3bcb44c06f7b92f3d8720aee523aa4f73
Leon Boussen <leon.boussen@hotmail.com>
2021-05-12 14:09:45 +0200

    improved player movement

---
+++ GroundControll/Assets/scripts/SpaceShipScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipScript : MonoBehaviour
+{
+    public float thrustSpeed = 15.0f;
+    public float thrustSpeedBackwards = -0.5f;
+    public float rotationSpeed = 3.0f;
+
+    private Rigidbody2D _rigidbody;
+    private bool _thrusting;
+    private bool _thrustingBack;
+    private float _turnDirection;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        _thrustingBack = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDirection = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _turnDirection = -1.0f;
+        }
+        else
+        {
+            _turnDirection = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thrusting)
+        {
+            _rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+
+        if (_thrustingBack)
+        {
+          //_rigidbody.AddForce(this.transform.up /this.thrustSpeedBackwards); dit werkt nog niet
+        }
+
+        if (_turnDirection != 0.0f)
+        {
+            _rigidbody.AddTorque(this.rotationSpeed * _turnDirection);
+        }
+    }
+}
