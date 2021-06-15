8cfaaea3e9cd76940383371b9f0a378170e00b79
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-06-15 12:44:21 +0200

    Got the interactive shop working. Needs work, but it's there to be refined - Mikey

---
+++ GroundControll/Assets/scripts/Planets/Player/ManMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManMovement : MonoBehaviour
+{
+    public Rigidbody2D _rigidbody2D;
+
+    private CharacterController player;
+
+    public float Speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(Speed, 0, 0 ) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-Speed, 0, 0) * Time.deltaTime;
+        }
+
+    }
+}
