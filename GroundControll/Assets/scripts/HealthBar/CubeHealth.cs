f853c6c6595aefb897a7a59842fcb6c01a8bc60e
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2021-06-21 11:43:24 +0200

    Testing for blackhole

---
+++ GroundControll/Assets/scripts/HealthBar/CubeHealth.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CubeHealth : MonoBehaviour
+{
+    public Slider AsteroidHPSlider;
+    private float TargetHealth;
+    private float SlidingTime = 0.2f;
+    public void SetMaxHealth(float health)
+    {
+        AsteroidHPSlider.maxValue = health;
+        AsteroidHPSlider.value = health;
+    }
+
+    public void SetTargetHealth(float health)
+    {
+        TargetHealth = health;
+        AsteroidHPSlider.DOValue(TargetHealth, SlidingTime);
+
+    }
+}
