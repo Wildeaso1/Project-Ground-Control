ed11fc86248f796676b4278d4a166ea01cd04ec6
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-06-24 10:59:14 +0200

    Spawning blackholes is underway -Mikey

--- GroundControll/Assets/scripts/Blackhole/BlackHole.cs
+++ GroundControll/Assets/scripts/Blackhole/BlackHole.cs
@@ -6,4 +6,12 @@ public class BlackHole : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Blackhole")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
 }
