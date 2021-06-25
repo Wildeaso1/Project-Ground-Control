85f19aef06de15ad0607746187525dbb182bf6a7
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2021-06-25 18:02:22 +0200

    Loading screen werkt

--- GroundControll/Assets/scripts/Planets/Area/SpaceTransition.cs
+++ GroundControll/Assets/scripts/Planets/Area/SpaceTransition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SpaceTransition : MonoBehaviour
 {
@@ -9,12 +10,15 @@ public class SpaceTransition : MonoBehaviour
     private bool range;
     public string Scene;
     public GameObject pressE;
+    public GameObject LoadingScreen;
+    public Slider Pslider;
 
     
 
     // Start is called before the first frame update
     void Start()
     {
+        LoadingScreen.SetActive(false);
         pressE.SetActive(false);
     }
 
@@ -24,7 +28,7 @@ public class SpaceTransition : MonoBehaviour
         if (range && Input.GetKeyDown(KeyCode.E))
         {
             Inventory.ScoreCredits = PlayerPrefs.GetInt("Credits");
-            SceneManager.LoadScene(Scene);
+            LoadLevel(1);
         }
     }
 
@@ -38,5 +42,25 @@ public class SpaceTransition : MonoBehaviour
     {
         range = false;
         pressE.SetActive(false);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncLevel(sceneIndex));
+    }
+
+
+    IEnumerator LoadAsyncLevel (int sceneIndex)
+    {
+        LoadingScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Pslider.value = progress;
+
+            yield return null;
+        }
     }
 }
